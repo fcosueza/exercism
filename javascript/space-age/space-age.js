4f76325f3ab81e7ad3db11fbd75f9afb7cdc02eb
Francisco Sueza <fcosueza@disroot.org>
2019-08-12 14:46:36 +0200

    New space-age solution added

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,16 +1,29 @@
-// Planet orbits periods in seconds (1 year =  31557600)
-const planetsOrbitsPeriods = {
-  earth: 31557600,
-  mercury: 7600543.81992,
-  venus: 19414149.052176,
-  mars: 59354032.690079994,
-  jupiter: 374355659.124,
-  saturn: 929292362.8848,
-  uranus: 2651370019.3296,
-  neptune: 5200418560.032001
+// Age's precision
+const precision = 2;
+
+// Earth orbital period expressed in seconds (1 year = 31557600 seconds)
+const earthOrbitPeriod = 31557600;
+
+// Planets orbital periods expressed in Earth years
+const planetsOrbitPeriod = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
 };
 
-const age = (planet, age) =>
-  Math.round((age / planetsOrbitsPeriods[planet]) * 100) / 100;
+// Function age
+const age = (planet, age) => {
+  let planetAge = age / earthOrbitPeriod;
+
+  if (planet != "earth") {
+    planetAge /= planetsOrbitPeriod[planet];
+  }
+
+  return Number.parseFloat(planetAge.toFixed(precision));
+};
 
 export { age };
