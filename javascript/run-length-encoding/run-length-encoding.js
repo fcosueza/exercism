4ba138b8147b5904bec6ed6121b455bb9e5cbd95
Francisco Sueza <fcosueza@disroot.org>
2019-09-11 14:09:18 +0200

    Refactoriced run-length-encoding

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,29 +1,7 @@
-const encode = string => {
-  if (string === "") return "";
+const encode = string =>
+  string.replace(/(.)\1*/g, (match, p1) => `${match.length === 1 ? "" : match.length}${p1}`);
 
-  const regex = /(.)\1*/g;
-
-  return string
-    .match(regex)
-    .map(elem => elem.length < 2 ? elem.charAt(0) : elem.length.toString() + elem.charAt(0))
-    .join("");
-};
-
-const decode = string => {
-  if (string === "") return "";
-
-  const termRegex = /\d+\D|\D/g;
-  const numsRegex = /\d+/g;
-  const charRegex = /\D/g;
-
-  return string
-    .match(termRegex)
-    .map(elem => {
-      let num = parseInt(elem.match(numsRegex));
-
-      return num ? new Array(num).fill(elem.match(charRegex)).join("") : elem;
-    })
-    .join("");
-};
+const decode = string =>
+  string.replace(/(\d+)(\D)/g, (_, p1, p2) => p2.repeat(p1));
 
 export { encode, decode };
