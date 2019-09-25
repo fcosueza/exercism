bc88f209030b4bf96a0bf12c9cfe259f2c39864d
Francisco Sueza <fcosueza@disroot.org>
2019-09-25 20:54:43 +0200

    Provisional roman numerals solution

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,3 +1,28 @@
-export const toRoman = () => {
-  throw new Error("Remove this statement and implement this function");
+const ROMAN = [
+  ["M", 1000],
+  ["CM", 900],
+  ["D", 500],
+  ["CD", 400],
+  ["C", 100],
+  ["XC", 90],
+  ["L", 50],
+  ["XL", 40],
+  ["X", 10],
+  ["IX", 9],
+  ["V", 5],
+  ["IV", 4],
+  ["I", 1]
+];
+
+const toRoman = number => {
+  if (!Number.isInteger(number) || number < 0 || number > 3000) return "";
+
+  return ROMAN.reduce((acc, curr) => {
+      const [result, next] = acc;
+      const [letter, value] = curr;
+
+      return [result + letter.repeat(next / value), next % value];
+    }, ["", number])[0];
 };
+
+export { toRoman };
