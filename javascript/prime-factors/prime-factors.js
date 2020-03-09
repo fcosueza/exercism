c620c3cdf73ab6ed865ae1d4781a2ddcf8cd95ba
Francisco Sueza <fcosueza@disroot.org>
2020-03-09 16:53:33 +0100

    Changed var name for another "more descriptive"

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,10 +1,10 @@
 export const primeFactors = n => {
   let factors = [];
 
-  for (let i = 2; n > 1; i++)
+  for (let div = 2; n > 1; div++)
     while (n % i === 0) {
-      factors.push(i);
-      n = n / i;
+      factors.push(divider);
+      n = n / div;
     }
 
   return factors;
