18106de3256df9aa17d7e311965427563858f475
Francisco Sueza <fcosueza@disroot.org>
2021-09-13 11:52:34 +0200

    Added temp solution

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -1,5 +1,19 @@
+const sound = {
+  3: "Pling",
+  5: "Plang",
+  7: "Plong"
+};
+
 const convert = number => {
-  if (number === 1) return number.toString();
+  let result = "";
+
+  if (number % 3 === 0) result += sound[3];
+  if (number % 5 === 0) result += sound[5];
+  if (number % 7 === 0) result += sound[7];
+
+  if (!result) result = number.toString();
+
+  return result;
 };
 
 export { convert };
